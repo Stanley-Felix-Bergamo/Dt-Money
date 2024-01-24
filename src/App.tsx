b528d118bb45a68afc73295b89e59429f0426be0
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-01-24 14:29:00 -0300

    Calculando as trasacoes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { Globalstyled } from "./styles/global";
 import { Transactions } from "./pages/Transactions";
+import { TransactionProvider } from "./contexts/transactionsContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Globalstyled />
-      <Transactions />
+      <TransactionProvider>
+        <Transactions />
+      </TransactionProvider>
     </ThemeProvider>
   );
 }
